210e37672ad94298aecd58aa2f1b1b4f33016f7d
Caroline Baites <carolinemv.le@gmail.com>
2023-11-07 15:42:22 -0800

    updates controllers

--- controllers/signs.js
+++ controllers/signs.js
@@ -1,24 +1,76 @@
-// const { sequelize } = require('../models');
 const db = require('../models');
 
 const createOne = async (req, res) => {
-  // TASK: Add sequelize function to add a new sign to the database
-};
-
-const getOne = async (req, res) => {
-
+  // TASK: Create function to add a new sign to the database
 };
 
 const getAll = async (req, res) => {
-  // TASK: Add sequelize function to get an array of all signs in the database
+  // TASK: Create function to get an array of all signs in the database
+};
+
+const getOne = async (req, res) => {
+  db.sign.findOne({
+    where: {
+      id: req.params.signId
+    }
+  }).then(sign => {
+    if (sign) res.status(200).send(sign);
+    else res.status(404).send('SIGN NOT FOUND');
+  }).catch(err => {
+    res.send(err);
+  });
 };
 
 const updateOne = async (req, res) => {
-
+  db.sign.findOne({
+    where: {
+      id: req.params.signId
+    }
+  }).then(sign => {
+    if (sign) {
+      db.sign.update(req.body, {
+        where: {
+          id: req.params.signId
+        },
+        returning: true,
+        plain: true
+      }).then(sign => {
+        if (sign) res.status(200).send(sign[1]);
+        else res.status(400).send('ERROR UPDATING SIGN');
+      }).catch(err => {
+        res.send(err);
+      })
+    } else {
+      res.status(404).send('SIGN NOT FOUND');
+    }
+  }).catch(err => {
+    res.send(err);
+  });
 };
 
 const deleteOne = async (req, res) => {
-
+  db.sign.findOne({
+    where: {
+      id: req.params.signId
+    }
+  }).then(sign => {
+    if (sign) {
+      db.sign.destroy({
+        where: {
+          id: req.params.signId
+        },
+        force: true
+      }).then(() => {
+        res.status(200).send('SUCCESSFUL DELETION');
+      }).catch(err => {
+        res.send(err);
+      });
+    } else {
+      res.status(404).send('SIGN NOT FOUND');
+    }
+  }).catch(err => {
+    res.send(err);
+  })
 };
 
 module.exports = {
