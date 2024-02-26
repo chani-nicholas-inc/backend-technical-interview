02a4beb505d7ea6f5417932328a9639a77d29bc2
Caroline Baites <carolinemv.le@gmail.com>
2024-02-26 09:19:17 -0800

    updates config vars

--- config/config.js
+++ config/config.js
@@ -2,17 +2,14 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.HEROKU_UN,
-    password: process.env.HEROKU_PW,
-    database: process.env.HEROKU_DB,
-    host: process.env.HEROKU_HOST,
+    username: process.env.LOCAL_UN,
+    password: process.env.LOCAL_PW,
+    database: process.env.LOCAL_DB,
+    host: process.env.LOCAL_HOST,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+      ssl: false
     }
   }
 }
